75b97e35af43464d4e3b6a93b4495b61963f7156
Daz <daz.wilkin@gmail.com>
2025-06-14 08:36:55 -0700

    Added MCP server and Prometheus metrics config

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -3,36 +3,81 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log/slog"
 )
 
+// Config is a type that represent the app's configuration
 type Config struct {
 	Prometheus string
-	Metrics    Metrics
+	Server     Server
+	Metric     Metric
 }
 
-func NewConfig() *Config {
-	addr := flag.String("metrics.addr", ":8080", "Endpoint on which metrics are published")
-	path := flag.String("metrics.path", "/metrics", "Path on which metrics are served")
+// NewConfig is a function that creates a new Config
+func NewConfig(logger *slog.Logger) (*Config, error) {
+	// MCP config
+	serverAddr := flag.String("server.addr", ":7777", "Endpoint on which MCP tools are published")
+	serverPath := flag.String("server.path", "/mcp", "Path on which MCP tools are served")
+
+	// Metrics config
+	metricAddr := flag.String("metric.addr", ":8080", "Endpoint on which metrics are published")
+	metricPath := flag.String("metric.path", "/metrics", "Path on which metrics are served")
+
+	// Prometheus server
 	prometheus := flag.String("prometheus", "http://localhost:9090", "Endpoint of Prometheus server")
+
 	flag.Parse()
+
+	if *prometheus == "" {
+		msg := "Flag '--prometheus' is required"
+		err := NewErrConfig(msg, nil)
+		logger.Error(msg, "err", err)
+		return nil, err
+	}
 
 	return &Config{
 		Prometheus: *prometheus,
-		Metrics: Metrics{
-			Addr: *addr,
-			Path: *path,
+		Server: Server{
+			Addr: *serverAddr,
+			Path: *serverPath,
 		},
-	}
+		Metric: Metric{
+			Addr: *metricAddr,
+			Path: *metricPath,
+		},
+	}, nil
 }
 
-type Metrics struct {
+// Server represents the MCP server's configuration
+// TODO(dazwilkin): Possibly unify with Metric type?
+type Server struct {
 	Addr string
 	Path string
 }
 
-func (m Metrics) GoString() string {
-	return fmt.Sprintf("Metrics{Endpoint: %q, Path: %q}", m.Addr, m.Path)
+// GoString is a method that generates a Go string
+func (m Server) GoString() string {
+	return fmt.Sprintf("MCP{Addr: %q, Path: %q}", m.Addr, m.Path)
 }
-func (m Metrics) String() string {
+
+// String is a method that generates a string
+func (m Server) String() string {
+	return fmt.Sprintf("%s/%s", m.Addr, m.Path)
+}
+
+// Metric is a type that represents the Prometheus metrics exporter configuration
+// TODO(dazwilkin): Possibly unify with MCP type?
+type Metric struct {
+	Addr string
+	Path string
+}
+
+// GoString is a method that returns a Go string
+func (m Metric) GoString() string {
+	return fmt.Sprintf("Metrics{Addr: %q, Path: %q}", m.Addr, m.Path)
+}
+
+// String is a method that returns a string
+func (m Metric) String() string {
 	return fmt.Sprintf("%s/%s", m.Addr, m.Path)
 }
