ede829dccca0bb407b4748a7e5a67b68b9940a57
Daz <daz.wilkin@gmail.com>
2025-06-13 19:28:37 -0700

    Simplify tool registration

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -37,82 +37,86 @@ func main() {
 		serverOpts...,
 	)
 
-	s.AddTool(
-		mcp.NewTool(
-			"alerts",
-			mcp.WithDescription("Prometheus Alerts"),
-		),
-		client.Alerts,
-	)
-	s.AddTool(
-		mcp.NewTool(
-			"metrics",
-			mcp.WithDescription("Prometheus Metrics"),
-		),
-		client.Metrics,
-	)
-	s.AddTool(
-		mcp.NewTool(
-			"query",
-			mcp.WithDescription("Prometheus Query"),
-			mcp.WithString("query",
-				mcp.Required(),
-				mcp.Description("Prometheus expression query string"),
+	tools := []server.ServerTool{
+		{
+			Tool: mcp.NewTool(
+				"alerts",
+				mcp.WithDescription("Prometheus Alerts"),
 			),
-			mcp.WithString("time",
-				mcp.Description("Evaluation timestamp (RFC-3339 or Unix)"),
+			Handler: client.Alerts,
+		},
+		{
+			Tool: mcp.NewTool(
+				"metrics",
+				mcp.WithDescription("Prometheus Metrics"),
 			),
-			mcp.WithString("timeout",
-				mcp.Description("Evaluation timeout"),
+			Handler: client.Metrics,
+		},
+		{
+			Tool: mcp.NewTool(
+				"query",
+				mcp.WithDescription("Prometheus Query"),
+				mcp.WithString("query",
+					mcp.Required(),
+					mcp.Description("Prometheus expression query string"),
+				),
+				mcp.WithString("time",
+					mcp.Description("Evaluation timestamp (RFC-3339 or Unix)"),
+				),
+				mcp.WithString("timeout",
+					mcp.Description("Evaluation timeout"),
+				),
+				mcp.WithNumber("limit",
+					mcp.Description("Maximum number of returned series"),
+				),
 			),
-			mcp.WithNumber("limit",
-				mcp.Description("Maximum number of returned series"),
+			Handler: client.Query,
+		},
+		{
+			Tool: mcp.NewTool(
+				"query_range",
+				mcp.WithDescription("Prometheus Query Range"),
+				mcp.WithString("query",
+					mcp.Required(),
+					mcp.Description("Prometheus expression query string"),
+				),
+				mcp.WithString("start",
+					mcp.Required(),
+					mcp.Description("Start timestamp (RFC-3339 or Unix)"),
+				),
+				mcp.WithString("end",
+					mcp.Required(),
+					mcp.Description("End timestamp (RFC-3339 or Unix)"),
+				),
+				mcp.WithString("step",
+					mcp.Required(),
+					mcp.Description("Query resolution step width in duration format"),
+				),
+				mcp.WithString("timeout",
+					mcp.Description("Evaluation timeout"),
+				),
+				mcp.WithNumber("limit",
+					mcp.Description("Maximum number of returned series"),
+				),
 			),
-		),
-		client.Query,
-	)
-	s.AddTool(
-		mcp.NewTool(
-			"query_range",
-			mcp.WithDescription("Prometheus Query Range"),
-			mcp.WithString("query",
-				mcp.Required(),
-				mcp.Description("Prometheus expression query string"),
+			Handler: client.QueryRange,
+		},
+		{
+			Tool: mcp.NewTool("rules",
+				mcp.WithDescription("Prometheus Rules"),
 			),
-			mcp.WithString("start",
-				mcp.Required(),
-				mcp.Description("Start timestamp (RFC-3339 or Unix)"),
+			Handler: client.Rules,
+		},
+		{
+			Tool: mcp.NewTool(
+				"targets",
+				mcp.WithDescription("Prometheus Targets"),
 			),
-			mcp.WithString("end",
-				mcp.Required(),
-				mcp.Description("End timestamp (RFC-3339 or Unix)"),
-			),
-			mcp.WithString("step",
-				mcp.Required(),
-				mcp.Description("Query resolution step width in duration format"),
-			),
-			mcp.WithString("timeout",
-				mcp.Description("Evaluation timeout"),
-			),
-			mcp.WithNumber("limit",
-				mcp.Description("Maximum number of returned series"),
-			),
-		),
-		client.QueryRange,
-	)
-	s.AddTool(
-		mcp.NewTool("rules",
-			mcp.WithDescription("Prometheus Rules"),
-		),
-		client.Rules,
-	)
-	s.AddTool(
-		mcp.NewTool(
-			"targets",
-			mcp.WithDescription("Prometheus Targets"),
-		),
-		client.Targets,
-	)
+			Handler: client.Targets,
+		},
+	}
+
+	s.AddTools(tools...)
 
 	stdioOpts := []server.StdioOption{}
 	slog.Info("StdioOptions", "opts", stdioOpts)
