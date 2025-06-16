e9fb107a523594a432969d5e1e28321f3a65fe7d
Daz <daz.wilkin@gmail.com>
2025-06-15 20:27:44 -0700

    Added `management` package tests

--- management/management_test.go
+++ management/management_test.go
@@ -1,6 +1,7 @@
 package management
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
 	"net/http/httptest"
@@ -8,51 +9,57 @@ import (
 	"testing"
 )
 
-func TestHealthy(t *testing.T) {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+var (
+	tests = []struct {
+		name string
+		// A clever way to reference a type's (Client's) methods
+		// Since both the handlers a func() int, we can generalize
+		handler func(*Client) int
+		want    int
+	}{
+		{
+			name:    "healthy",
+			handler: (*Client).Healthy,
+			want:    http.StatusOK,
+		},
+		{
+			name:    "ready",
+			handler: (*Client).Ready,
+			want:    http.StatusOK,
+		},
+	}
+)
 
-	mux := http.NewServeMux()
-	ts := httptest.NewServer(mux)
-	defer ts.Close()
-
-	mux.HandleFunc("/-/healthy", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
-
-	url := ts.URL
-
-	client := NewClient(url, logger)
-	got := client.Healthy()
-	want := http.StatusOK
-
-	if got != want {
-		t.Errorf("got: %s (%d); want: %s (%d)",
-			http.StatusText(got), got,
-			http.StatusText(want), want,
-		)
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	if _, err := w.Write([]byte("OK")); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 }
-func TestReady(t *testing.T) {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+func TestManagement(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	mux := http.NewServeMux()
-	ts := httptest.NewServer(mux)
-	defer ts.Close()
+			mux := http.NewServeMux()
+			ts := httptest.NewServer(mux)
+			defer ts.Close()
 
-	mux.HandleFunc("/-/ready", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
-	})
+			pattern := fmt.Sprintf("GET /-/%s", test.name)
+			mux.HandleFunc(pattern, okHandler)
+			url := ts.URL
 
-	url := ts.URL
+			client := NewClient(url, logger)
+			// The corresponding way pass the receiver (*Client)
+			got := test.handler(client)
+			want := test.want
 
-	client := NewClient(url, logger)
-	got := client.Ready()
-	want := http.StatusOK
-
-	if got != want {
-		t.Errorf("got: %s (%d); want: %s (%d)",
-			http.StatusText(got), got,
-			http.StatusText(want), want,
-		)
+			if got != want {
+				t.Errorf("got: %s (%d); want: %s (%d)",
+					http.StatusText(got), got,
+					http.StatusText(want), want,
+				)
+			}
+		})
 	}
 }
